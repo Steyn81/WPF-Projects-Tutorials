7812dc44cda5006fc1eb1935dcbfeed280a9ffe3
Deon Steyn <dsteyn076@gmail.com>
2021-10-10 15:38:27 +0200

    New Project
    Create a Windows Store Clone

---
+++ WindowStoreClone/UserControls/AppDetailsTabContent/Overview.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowStoreClone.UserControls.AppDetailsTabContent
+{
+
+    public partial class Overview : UserControl
+    {
+        public delegate void OnAppDetailsAppClicked(AnApp sender, RoutedEventArgs e);
+        public event OnAppDetailsAppClicked AppClicked;
+        public Overview()
+        {
+            InitializeComponent();
+            AppsViewerInsideOwerviewTab.AppClicked += AppsViewerInsideOwerviewTab_AnAppClicked;
+        }
+
+        private void AppsViewerInsideOwerviewTab_AnAppClicked(AnApp sender, RoutedEventArgs e)
+        {
+            AppClicked(sender, e);
+        }
+    }
+}
---
+++ WindowStoreClone/UserControls/AppDetailsTitleAndBackground.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WindowStoreClone.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AppDetailsTitleAndBackground.xaml
+    /// </summary>
+    public partial class AppDetailsTitleAndBackground : UserControl
+    {
+        public delegate void OnBackButtonClicked(object sender, RoutedEventArgs e);
+        public event OnBackButtonClicked BackButtonClicked;
+
+        public AppDetailsTitleAndBackground()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Btn(object sender, RoutedEventArgs e)
+        {
+            BackButtonClicked(sender, e);
+        }
+    }
+}
